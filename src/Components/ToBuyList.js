5d3b630b805437b768e19951c58f42cb17164021
PaulDreyer09 <pauljacobusdreyer@gmail.com>
2021-06-08 22:35:31 +0200

    addItem now displays a form

--- src/Components/ToBuyList.js
+++ src/Components/ToBuyList.js
@@ -5,17 +5,25 @@ const ToBuyList = (props) =>
     
     <div className='listHeader row'>
         <h4 className='col-8'>{props.list.name}</h4>
-        {
-            //Display a form to add a new item only after Add Item is clicked
-            //Also close all other forms
-        //props.handleTBLAddItemButton(props.list.id)
-            props.list.displayForm ? <p>Hi ho</p> : <></>
-        }
+        
         <a onClick={props.handleTBLAddItemButton(props.list.id)} className='addItemButton col-3 button'>
             Add Item
         </a>
         <i className='fa fa-angle-down col-1 button'></i>
     </div>
+    {
+            //Display a form to add a new item only after Add Item is clicked
+            //Also close all other forms
+            props.list.displayForm ? 
+            <div className='listItemForm'>
+                <form onSubmit=''>
+                    <label htmlFor='formName'>Item Name</label>
+                    <input type='text' id='formName' placeholder='Enter item name'></input>
+                    <input type='submit' value='Submit'/>
+                </form>
+            </div>
+            : <></>
+    }
     <div className='listContent'>
         {props.list.items.map((listItem) => (<ListItem key={listItem.id.toString()} listItem={listItem}/>))}
     </div>
